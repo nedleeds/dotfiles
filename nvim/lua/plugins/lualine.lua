926406c0e27d27c99fa69468e30ee0b6ac96389a
nedleeds <lllee.dh@gmail.com>
2024-08-16 17:56:32 +0900

    fix: insert mode color modify

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,7 +3,8 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
   config = function()
     local colors = {
-      blue = "#3fa3ff",
+      blue = "#80a0ff",
+      blue2 = "#3fa3ff",
       cyan = "#79dac8",
       green = "#33997f",
       black = "#080808",
@@ -17,16 +18,24 @@ return {
 
     local bubbles_theme = {
       normal = {
-        a = { fg = colors.black, bg = colors.green, gui = "bold" },
+        a = { fg = colors.black, bg = colors.green },
         b = { fg = colors.white, bg = colors.grey },
         c = { fg = colors.white },
+        y = { fg = colors.white, bg = colors.grey },
       },
-      insert = { a = { fg = colors.black, bg = colors.blue2, gui = "bold" } },
-      visual = { a = { fg = colors.black, bg = colors.organge, gui = "bold" } },
-      replace = { a = { fg = colors.black, bg = colors.red, gui = "bold" } },
+
+      insert = {
+        a = { fg = colors.black, bg = colors.blue2 },
+        b = { fg = colors.white, bg = colors.grey },
+        c = { fg = colors.white },
+        y = { fg = colors.white, bg = colors.grey },
+      },
+      visual = { a = { fg = colors.black, bg = colors.organge } },
+      replace = { a = { fg = colors.black, bg = colors.red } },
+
       inactive = {
-        a = { fg = colors.white, gui = "bold" },
-        b = { fg = colors.white },
+        a = { fg = colors.white, bg = colors.black },
+        b = { fg = colors.white, bg = colors.black },
         c = { fg = colors.white },
       },
     }
@@ -41,7 +50,7 @@ return {
         lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
         lualine_b = { "branch", "diff", "diagnostics" },
         lualine_c = { { "filename", separator = { right = "" } } },
-        lualine_x = { { "encoding", "fileformat", "filetype", color = { fg = colors.organge } } },
+        lualine_x = { { "encoding", "fileformat", "filetype", color = { fg = "#ff9e64" } } },
         lualine_y = { "progress" },
         lualine_z = {
           { "location", separator = { right = "" }, left_padding = 2 },
@@ -61,7 +70,7 @@ return {
 
     -- 하이라이트 그룹 설정
     vim.cmd([[
-      hi CursorLineNr guifg=#FFA500 guibg=NONE gui=bold
+      hi CursorLineNr guifg=#FFA500 guibg=NONE gui=bold,
     ]])
   end,
 }
