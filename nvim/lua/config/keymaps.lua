0f4724b5752c11cd17b710eac7711afcbdb391c5
nedleeds <lllee.dh@gmail.com>
2026-01-28 23:17:17 +0900

    keymap: <leader>q - Quit / Close windows
    (cherry picked from commit 874a28daec735ff777fa5a07e6687679ada74699)

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -16,6 +16,7 @@ local function with_require(mod, fn, notify_name)
   end
 end
 
+local util = require("config.util")
 -- ---------------------------------------------------------
 -- which-key groups + icons (v3 icon table)
 -- ---------------------------------------------------------
@@ -43,7 +44,7 @@ do
 
       -- Singles (top-level)
       { "<leader>e", desc = "Explorer",           icon = { icon = " ",  hl = "WKIconExplorer" } },
-      { "<leader>q", desc = "Quit",               icon = { icon = "󰗼 ", hl = "WKIconFile" } },
+      { "<leader>q", desc = "Quit Window",        icon = { icon = "󰗼 ", hl = "WKIconFile" } },
       { "<leader>T", desc = "Retab",              icon = { icon = "󰉢 ", hl = "WKIconFormat" } },
 
       -- Non-leader keymaps (shown with ?)
@@ -86,7 +87,8 @@ end, "Log: snacks history")
 -- ---------------------------------------------------------
 map("n", "<C-[>", "<Cmd>nohlsearch<CR><Esc>", "Search: Clear highlight")
 map("n", "<leader>w", "<Cmd>w<CR>", "File: Save")
-map("n", "<leader>q", "<Cmd>close<CR>", "Quit Window")
+
+map("n", "<leader>q", util.smart_close_window, "Close window / Quit")
 map("n", "<leader>T", "<Cmd>retab<CR>", "Format: Retab")
 
 -- LSP format (global)
--- nvim/lua/config/util.lua
+++ nvim/lua/config/util.lua
@@ -17,4 +17,37 @@ function M.is_client_attached(name, bufnr)
   return false
 end
 
+function M.smart_close_window()
+  local win = vim.api.nvim_get_current_win()
+  local buf = vim.api.nvim_win_get_buf(win)
+
+  local bt = vim.bo[buf].buftype
+
+  -- -------------------------------------------------
+  -- special windows: always just close
+  -- (dap, help, terminal, snacks, floats, etc)
+  -- -------------------------------------------------
+  if bt ~= "" then
+    pcall(vim.api.nvim_win_close, win, true)
+    return
+  end
+
+  -- -------------------------------------------------
+  -- normal windows
+  -- -------------------------------------------------
+  local wins = vim.api.nvim_tabpage_list_wins(0)
+
+  if #wins > 1 then
+    pcall(vim.api.nvim_win_close, win, false)
+    return
+  end
+
+  if #vim.api.nvim_list_tabpages() > 1 then
+    vim.cmd("tabclose")
+    return
+  end
+
+  vim.cmd("qa")
+end
+
 return M
